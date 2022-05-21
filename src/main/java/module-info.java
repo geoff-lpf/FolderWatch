9b03500aa237ad5be1ff2aa235bb0dc35c1be112
Geoff <geoff.lai@zoho.com>
2022-05-22 03:52:23 +0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module work.geoff.folderwatch {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens work.geoff.folderwatch to javafx.fxml;
+    exports work.geoff.folderwatch;
+}
---
+++ src/main/java/work/geoff/folderwatch/HelloController.java
@@ -0,0 +1,14 @@
+package work.geoff.folderwatch;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
